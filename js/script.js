4e47808c4c0b649195e0e1868533d8370f799c1a
spacemanfromul <47415842+spacemanfromul@users.noreply.github.com>
2022-05-27 17:36:59 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -32,8 +32,8 @@ if (isMobile.any()) {
   let menuArrows = document.querySelectorAll('.menu_arrow');
   if (menuArrows.length > 0) {
     for (let index = 0; index < menuArrows.length; index++){
-      const menuArrow = menuArrows[index];
-      menuArrow.addEventListener("click", function(e){
+      const menuArrow = menuArrow[index];
+      menuArrow.addEventListener("click", function (e){
         menuArrow.parentElement.classList.toggle('_active');
       });
     }
@@ -44,4 +44,15 @@ else
 {
   document.body.classList.add('_pc');
 }
-  
+
+// Меню бургер
+const iconMenu = document.querySelector('.menu_icon');
+if (iconMenu) {
+  const menuBody = document.querySelector('.menu_body');
+  iconMenu.addEventListener("click", function (e){
+    document.body.classList.toggle('_lock');
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+  });
+
+}
